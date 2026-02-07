adce38ddf3bde67712c76047250a7d5f308ccf36
DavidDark <link100david@hotmail.com>
2026-02-07 16:22:43 -0600

    update on area and ubicacion fields2

--- routes/product.js
+++ routes/product.js
@@ -52,36 +52,36 @@ router.get('/find/:nombre', [], async (req, res) => {
         {
             nombre: "MCCAIN PAPA FRANCESA 3/8 SIGNATURE C/2.27 KG",
             existencias: 200,
-            area: ["congelador"],
-            ubicacion: ["-"],
+            area: "congelador",
+            ubicacion: "-",
             matriz: 0
         },
         {
             nombre: "REYMA CONTENEDOR 7X7 LISO HC771 PZ",
             existencias: 300,
-            area: ["desechables", "tienda"],
-            ubicacion: ["N2","R0-N4|R1-N4"],
+            area: "desechables|tienda",
+            ubicacion: "N2|R0-N4|R1-N4",
             matriz: 300
         },
         {
             nombre: "VIRMAR ADEREZO SABOR QUESO 1.1 KG PZ",
             existencias: 0,
-            area: ["-"],
-            ubicacion: ["-"],
+            area: "-",
+            ubicacion: "-",
             matriz: 10
         },
         {
             nombre: "BOLSA CAMISETA CHICA 25X40",
             existencias: 500,
-            area: ["proveedores","tienda"],
-            ubicacion: ["R7-N0","R0-N1"],
+            area: "proveedores|tienda",
+            ubicacion: "R7-N0|R0-N1",
             matriz: 200
         },
         {
             nombre: "BOLSA CAMISETA MEDIANA 25X50",
             existencias: 100,
-            area: ["tienda"],
-            ubicacion: ["R0-N1"],
+            area: "tienda",
+            ubicacion: "R0-N1",
             matriz: 300
         },
     ];
